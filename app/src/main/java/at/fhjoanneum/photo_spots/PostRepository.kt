2376e36b9f91626052adca27d67bdaffa7de003e
Matthias Windisch <matthias@hwm.at>
2022-01-16 15:16:24 +0100

    #14 list own posts implemented

--- app/src/main/java/at/fhjoanneum/photo_spots/PostRepository.kt
+++ app/src/main/java/at/fhjoanneum/photo_spots/PostRepository.kt
@@ -29,6 +29,26 @@ object PostRepository {
 
         })
     }
+
+    fun getMyPhotoList(context: Context, success: (lessonList: List<PostModel>) -> Unit, error: (errorMessage: String) -> Unit) {
+        val token = getLoginToken(context)
+        Api.retrofitService.getMyPhotos(token).enqueue(object : Callback<List<PostModel>> {
+            override fun onFailure(call: Call<List<PostModel>>, t: Throwable) {
+                error("The call failed")
+            }
+
+            override fun onResponse(call: Call<List<PostModel>>, response: Response<List<PostModel>>) {
+                val responseBody = response.body()
+                if (response.isSuccessful && responseBody != null) {
+                    success(responseBody)
+                } else {
+                    error(response.message().toString())
+                }
+            }
+
+        })
+    }
+
     private fun getLoginToken(context: Context):String {
         val applicationContext = context.applicationContext
         val db = LoginModelDatabase.getDatabase(applicationContext)
--- app/src/main/java/at/fhjoanneum/photo_spots/ui/home/HomeFragment.kt
+++ app/src/main/java/at/fhjoanneum/photo_spots/ui/home/HomeFragment.kt
@@ -1,7 +1,9 @@
 package at.fhjoanneum.photo_spots.ui.home
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +11,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import at.fhjoanneum.photo_spots.CameraActivity
+import at.fhjoanneum.photo_spots.PostAdapter
+import at.fhjoanneum.photo_spots.PostRepository
 import at.fhjoanneum.photo_spots.R
 import at.fhjoanneum.photo_spots.databinding.FragmentHomeBinding
 
@@ -20,7 +26,9 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    val postAdapter = PostAdapter() {
 
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -38,8 +46,34 @@ class HomeFragment : Fragment() {
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (activity?.isDestroyed == false && this.isAdded && view != null){
+            updateList(view?.context!!)
+
+
+            val recyclerView = view?.findViewById<RecyclerView>(R.id.home_recyclerview)
+            recyclerView?.layoutManager = LinearLayoutManager(view?.context)
+            recyclerView?.adapter = postAdapter
+        }
+
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun updateList(context: Context){
+        PostRepository.getMyPhotoList(context,
+            success = {
+                // handle success
+                postAdapter.updateList(it)
+            },
+            error = {
+                // handle error
+                Log.e("API ERROR",it)
+            }
+        )
     }
 }
