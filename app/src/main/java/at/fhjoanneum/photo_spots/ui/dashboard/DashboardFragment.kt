f92d8f5bc56b5d66dae2232d2c8d56ca5ee6abd6
sahra-marie <sahra-marie.kreidl@edu.fh-joanneum.at>
2022-01-25 23:39:03 +0100

    #2 added searchbar to search for post title in dashboard

--- app/src/main/java/at/fhjoanneum/photo_spots/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/at/fhjoanneum/photo_spots/ui/dashboard/DashboardFragment.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -17,6 +18,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import at.fhjoanneum.photo_spots.*
 import at.fhjoanneum.photo_spots.databinding.FragmentDashboardBinding
+import org.w3c.dom.Text
+import java.util.*
 
 class DashboardFragment : Fragment() {
 
@@ -25,6 +28,9 @@ class DashboardFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private var postList = listOf<PostModel>()
+    private var filteredList = mutableListOf<PostModel>()
 
     val postAdapter = PostAdapter() {
 
@@ -59,17 +65,52 @@ class DashboardFragment : Fragment() {
             recyclerView?.adapter = postAdapter
         }
 
+        var searchView = view?.findViewById<SearchView>(R.id.dashboard_search)
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(query:String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if(newText!!.isNotEmpty()){
+                    filteredList.clear()
+                    var search = newText.toLowerCase(Locale.getDefault())
+
+                    for (post in postList) {
+                        // TODO: currently one can't search by category because it doesn't get saved for the post
+                        /*
+                        for (category in post.Title) {
+                            if (category.toLowerCase(Locale.getDefault()).contains(search)) {
+                                filteredList.add(post)
+                            }
+                        }*/
+                        if (post.Title.toLowerCase(Locale.getDefault()).contains(search)) {
+                            filteredList.add(post)
+                        }
+                    }
+                    postAdapter.updateList(filteredList)
+                }
+                else {
+                    postAdapter.updateList(postList)
+                }
+                return true
+            }
+
+        })
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-    fun updateList(context: Context){
+
+    private fun updateList(context: Context){
         PostRepository.getphotoList(context,
             success = {
                 // handle success
                 postAdapter.updateList(it)
+                postList = it
             },
             error = {
                 // handle error
